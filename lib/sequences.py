80b8f9aa08980f324809ef768c93ffc3c61833ee
Brenden Hartman <brendenhart22@gmail.com>
2024-04-25 14:16:40 -0500

    created a fibbonachi sequence generator with lenght parameter getting to know sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length > 0:
+        if length == 1:
+            fib_list = [0]
+        elif length == 2:
+            fib_list = [0,1]
+        else:
+            fib_list = [0,1]
+            i = 0
+            for i in range(length - 2):
+                fib_list.append(fib_list[i] + fib_list[i + 1])
+    print(fib_list)
